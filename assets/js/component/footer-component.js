5af2a8183a0ccf24fdf53fb78c286ce0d3864eb7
Ariel Armijo <arielarmijo@yahoo.es>
2021-12-22 10:32:43 -0300

    update footer

--- assets/js/component/footer-component.js
+++ assets/js/component/footer-component.js
@@ -4,6 +4,11 @@ export class FooterComponent extends Component {
 
   template = `
     <h4>Ariel Armijo 2021</h4>
+    <div>
+      <a href="https://www.linkedin.com/in/arielarmijo" target="_blank"><i class="fab fa-linkedin zoom"></i></a>
+      <a href="https://github.com/arielarmijo" target="_blank"><i class="fab fa-github-square zoom"></i></a>
+      <a href="mailto:arielarmijo@yahoo.es"><i class="fas fa-envelope zoom"></i></a>
+    </div>
   `;
 
   constructor(tag) {
